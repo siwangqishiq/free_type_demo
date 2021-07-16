41aa2a968ec0974706f4ec1226b6dd4e788f4eb3
siwangqishiq <525647740@qq.com>
2021-07-16 14:19:20 +0800

    chinese char

--- main.cpp
+++ main.cpp
@@ -3,25 +3,21 @@
 #include "ft2build.h"
 #include FT_FREETYPE_H  
 
-int main(){
-    FT_Library ftLib;
-    if (FT_Init_FreeType(&ftLib)){
-        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-        return -1;
-    }
-
+int printChar(FT_Library *ftLib , wchar_t wChar){
     FT_Face face;
-    if (FT_New_Face(ftLib, "kaiti.ttf", 0, &face)){
+    if (FT_New_Face(*ftLib, "songti.ttc", 0, &face)){
         std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
         return -1;
     }
     
     FT_Set_Pixel_Sizes(face , 0 , 32);
 
-    if (FT_Load_Char(face, 'B', FT_LOAD_RENDER)){
+    if (FT_Load_Char(face, wChar, FT_LOAD_RENDER)){
         std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
         return -1;
     }
+    
+
 
     std::cout << "width = " << face->glyph->bitmap.width << std::endl;
     std::cout << "height = " << face->glyph->bitmap.rows << std::endl;
@@ -37,30 +33,31 @@ int main(){
 
     for(int i = 0 ;  i < fontBit.rows ; i++ ){
         for(int j = 0 ; j < fontBit.width ; j++){
-            std::cout << ((fontBit.buffer[i * fontBit.pitch + j] == 0)?" ":"x");
+            std::cout << ((fontBit.buffer[i * fontBit.pitch + j] == 0)?" ":"+");
         }
         std::cout<< std::endl;
     }
 
     FT_Done_Face(face);
-    FT_Done_FreeType(ftLib);
+
     return 0;
 }
 
+int main(){
+    FT_Library ftLib;
+    if (FT_Init_FreeType(&ftLib)){
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+    
+    std::wstring str = L"你A";
 
-/**
- * 
- * 
- * xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-
- * */
+    //auto p = wStr.c_str();
+    for(int i = 0 ; i < str.size();i++){
+        printChar(&ftLib , str[i]);
+        // std::cout << p[i] << std::endl;
+    }//end for i
+    
+    FT_Done_FreeType(ftLib);
+    return 0;
+}
